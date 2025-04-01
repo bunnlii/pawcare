0f0678e46220f0d29fd3d419e563e69addc47409
bunnlii <libbzss7@gmail.com>
2025-04-01 13:18:58 -0400

    updated

--- src/main/java/com/Pawcare/_0/Statistics/StatisticsController.java
+++ src/main/java/com/Pawcare/_0/Statistics/StatisticsController.java
@@ -1,18 +1,27 @@
 package com.Pawcare._0.Statistics;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/statistics")
 
 public class StatisticsController {
+
+    @Autowired
+    private StatisticsService StatisticsService;
+
     @GetMapping("/all")
-    public Object getStatistics() {
-        return new ResponseEntity<>(getStatistics().getClass(), HttpStatus.OK);
+    public ResponseEntity<List<Statistics>> getAllStatistics() {
+        List<Statistics> statisticsList = StatisticsService.getStatistics();
+        return ResponseEntity.ok(statisticsList);
     }
+
 
 }
