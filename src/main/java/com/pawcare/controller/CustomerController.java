0fa49bb73e92c01d7d0dc3e1d973242607da0449
Yasmine Chiboub <ymchiboub@uncg.edu>
2025-04-02 18:47:45 -0400

    Final Customer Api

--- src/main/java/com/pawcare/controller/CustomerController.java
+++ src/main/java/com/pawcare/controller/CustomerController.java
@@ -13,5 +13,30 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return customerService.createCustomer(customer);
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return CustomerService.getAllCustomers;
+    }
+
+    @GetMapping("/{id}")
+    public Customer getCustomerById(@PathVariable Long id) {
+        return customerService.getCustomerById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Customer updateCustomer(@PathVariable Long id, @RequestBody Customer updated) {
+        return customerService.updateCustomer(id, updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+    }
+
 
 }
--- src/main/java/com/pawcare/service/CustomerService.java
+++ src/main/java/com/pawcare/service/CustomerService.java
@@ -1,8 +1,45 @@
 package com.pawcare.service;
 
+import com.pawcare.entity.Customer;
+import com.pawcare.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
 
+    public static List<Customer> getAllCustomers;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public List<Customer> getAllCustomers(){
+        return customerRepository.findAll();
+    }
+
+    public Customer getCustomerById(Long id) {
+        return customerRepository.findById(id).orElse(null);
+    }
+
+    public Customer updateCustomer(Long id, Customer updated) {
+        Customer customer = customerRepository.findById(id).orElse(null);
+        if (customer != null) {
+            customer.setName(updated.getName());
+            customer.setEmail(updated.getEmail());
+            customer.setPhone(updated.getPhone());
+            customer.setAddress(updated.getAddress());
+            return customerRepository.save(customer);
+        }
+        return null;
+    }
+
+    public void deleteCustomer(Long id) {
+        customerRepository.deleteById(id);
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
 }
