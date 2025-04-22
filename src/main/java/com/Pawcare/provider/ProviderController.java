bd9b731ceca38a5ef926d5f9fd43b07c30885274
bunnlii <libbzss7@gmail.com>
2025-04-22 03:43:25 -0400

    provider create/delete/updates

--- src/main/java/com/Pawcare/provider/ProviderController.java
+++ src/main/java/com/Pawcare/provider/ProviderController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -16,20 +17,48 @@ public class ProviderController {
     private ProviderService service;
 
     @GetMapping("/all")
-    public Object getAllProvider() {
-        return new ResponseEntity<>(service.getAllProvider(), HttpStatus.OK);
+    public Object getAllProvider(Model model) {
+        //return new ResponseEntity<>(service.getAllProvider(), HttpStatus.OK);
+        model.addAttribute("providerList", service.getAllProvider());
+        model.addAttribute("title", "All Providers");
+        return "provider-list";
     }
+
+    @GetMapping("/{providerID}")
+    public Object getProviderByID(@PathVariable int providerID, Model model) {
+        model.addAttribute("provider", service.getProviderByID(providerID));
+        model.addAttribute("title", "Provider #: " + providerID);
+        return "provider-details";
+    }
+
 
     @PostMapping("/new")
-    public Object addNewProvider(@RequestBody Provider provider) {
-        service.addNewProvider(provider);
-        return new ResponseEntity<>(service.getAllProvider(), HttpStatus.CREATED);
+    public Object addNewProvider(Provider provider) {
+        Provider savedProvider = service.addNewProvider(provider);
+        return "redirect:/provider/" + savedProvider.getProviderID();
     }
 
-    @PutMapping("/update/{providerID}")
-    public Object updateProvider(@PathVariable int providerID, @RequestBody Provider provider) {
+
+    @GetMapping("/createForm")
+    public String showCreateForm(Model model) {
+        Provider provider = new Provider();
+        model.addAttribute("provider", provider);
+        model.addAttribute("title", "Create New Provider");
+        return "provider-create";
+    }
+
+
+    @GetMapping("/update/{providerID}")
+    public Object updateProvider(@PathVariable int providerID, Model model) {
+        model.addAttribute("provider", service.getProviderByID(providerID));
+        model.addAttribute("title", "Update Provider");
+        return "provider-update";
+    }
+
+    @PostMapping("/update/{providerID}")
+    public Object updateProvider(@PathVariable int providerID, Provider provider) {
         service.updateProvider(providerID, provider);
-        return new ResponseEntity<>(service.getProviderById(providerID), HttpStatus.OK);
+        return "redirect:/provider/" + providerID;
     }
 
     @DeleteMapping("/delete/{providerID}")
