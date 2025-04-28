a1a94d94b6d8cb32fef901f018f798a50a519759
Yasmine Chiboub <ymchiboub@uncg.edu>
2025-04-28 14:50:03 -0400

    usecases n ready fo provdier connect

--- src/main/java/com/Pawcare/repository/ServiceRepository.java
+++ src/main/java/com/Pawcare/repository/ServiceRepository.java
@@ -2,9 +2,11 @@ package com.pawcare.repository;
 
 import com.pawcare.entity.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ServiceRepository extends JpaRepository<Service, Long> {
     // List<Service> findByProviderId(Long providerId);
 }
